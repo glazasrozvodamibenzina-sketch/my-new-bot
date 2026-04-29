931b61c8877a9993e2fba151dc77b6bbf5578c96
glazasrozvodamibenzina-sketch <glazasrozvodamibenzina@gmail.com>
2026-04-29 17:07:50 +0200

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,12 +1,11 @@
-mport asyncio
+import asyncio
 import random
 from highrise import BaseBot, User, Position
-from highrise.main import main
+from highrise.__main__ import main
 
 class MyBot(BaseBot):
-    def init(self):
-        super().init()
-        # Список твоих приветствий
+    def __init__(self):
+        super().__init__()
         self.welcome_messages = [
             "Привет! Я так рад тебя видеть, проходи, располагайся! ✨",
             "Оу, привет-привет! Рад тебя видеть, давай общаться! 😊",
@@ -18,19 +17,15 @@ class MyBot(BaseBot):
         print("✅ Бот запущен!")
 
     async def on_user_join(self, user: User, position: Position):
-        # Выбираем случайную фразу
         message = random.choice(self.welcome_messages)
-        # Бот пишет в чат (подставляем имя игрока, если хочешь, или просто фразу)
         await self.highrise.chat(message)
-        # Бот машет рукой
         try:
             await self.highrise.send_emote("emote-hello", user.id)
         except:
             pass
 
-if name == "main":
+if __name__ == "__main__":
     room_id = "69ee35fab6bcfa4b70966bac"
     token = "93356fc362c144b1364b9b56314cd27400ad3d7737a7eeff88758290dbbae28d"
-    
     bot = MyBot()
     asyncio.run(main([bot], room_id, token))
